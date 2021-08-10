8df0a5246414edc76b3164818f47ec7e2b904a21
Yui Kondo <yuikondobook@gmail.com>
2021-08-10 21:26:47 +0900

    migration fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_10_111310) do
-
+ActiveRecord::Schema.define(version: 2021081101211536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.integer "unit_price"
+    t.date "expiry_date"
+    t.integer "weight"
+    t.integer "stock_amount"
+    t.integer "unit"
+    t.integer "discount_rate"
+    t.integer "public_status"
+    t.text "description"
+    t.integer "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer "total_price"
     t.string "pay_method"
     t.string "status"
-    t.bigint "buyer_id", null: false
+    t.bigint "buyer_id_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["buyer_id_id"], name: "index_orders_on_buyer_id_id"
@@ -51,5 +85,6 @@ ActiveRecord::Schema.define(version: 2021_08_10_111310) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "orders", "users", column: "buyer_id_id"
 end
