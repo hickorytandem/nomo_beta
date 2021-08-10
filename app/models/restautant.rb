d736fe6081b2c1e1302f1176cdae5ae8ee4ffe2a
Namkhing Lee <nonlapat.lee@gmail.com>
2021-08-10 18:30:13 +0700

    Create restaurant model

---
+++ app/models/restautant.rb
@@ -0,0 +1,4 @@
+class Restautant < ApplicationRecord
+  validates :name, :closing_hours, :opening_hours, presence: true
+  has_many :users
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_10_063630) do
+ActiveRecord::Schema.define(version: 2021_08_10_112722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restautants", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
+    t.string "catogory"
+    t.integer "opening_hours"
+    t.integer "closing_hours"
+    t.string "payment_method"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
