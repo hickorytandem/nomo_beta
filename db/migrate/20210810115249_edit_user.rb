56901d68af86e745048d342257d44d8804985a03
Namkhing Lee <nonlapat.lee@gmail.com>
2021-08-10 19:38:32 +0700

    change user model

--- db/migrate/20210810115249_edit_user.rb
+++ db/migrate/20210810115249_edit_user.rb
@@ -1,4 +1,5 @@
 class EditUser < ActiveRecord::Migration[6.0]
   def change
+    add_column :users, :address, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_10_113406) do
+ActiveRecord::Schema.define(version: 2021_08_10_122709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,8 +35,14 @@ ActiveRecord::Schema.define(version: 2021_08_10_113406) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
+    t.string "name"
+    t.string "phone_number"
+    t.string "card_detail"
+    t.bigint "restaurant_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["restaurant_id"], name: "index_users_on_restaurant_id"
   end
 
 end
