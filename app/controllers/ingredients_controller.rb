ff98009b973a928d4d4132e5862bbc5f4a3473b0
Anna Nonaka <annanonaka@gmail.com>
2021-08-11 12:11:26 +0900

    created show action and a view

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,46 @@
+class IngredientsController < ApplicationController
+  before_action :find_ingredient, only: [:show]
+
+  def index
+
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    # authorize @ingredient
+    if @ingredient.save
+      redirect_to ingredients_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def find_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :photo, :unit_price, :expiry_date, :weight, :stock_amount, :unit, :discount_rate, :publish_status, :description, :status)
+  end
+end
