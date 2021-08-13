5559558947d1d1ecf62b2f9ad4b3e5901b7368b8
Namkhing Lee <nonlapat.lee@gmail.com>
2021-08-13 11:04:10 +0700

    Add pundit to ingredient

---
+++ app/policies/ingredient_policy.rb
@@ -0,0 +1,28 @@
+class IngredientPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def edit?
+    record.user == user
+  end
+
+  def update?
+    edit?
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+end
