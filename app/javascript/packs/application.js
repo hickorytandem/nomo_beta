0609319de29b96bef6ea1f8bf1e1752615c468c6
Namkhing Lee <nonlapat.lee@gmail.com>
2021-08-17 23:00:17 +0700

    add side nav

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -57,6 +57,8 @@ document.addEventListener('turbolinks:load', () => {
 
 });
 
+// ---------------------------------------------
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -81,4 +83,18 @@ window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+
+// ------------------------------------------------------------------------------
+
+// Sidenav for Cart
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
