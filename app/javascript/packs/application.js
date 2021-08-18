11fcaad11e8b1f83d3c9d7737bd0892a1a6f7bac
Namkhing Lee <nonlapat.lee@gmail.com>
2021-08-18 23:49:17 +0700

    Customize order sidebar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -89,12 +89,12 @@ window.onclick = function (event) {
 
 // Sidenav for Cart
 
-/* Set the width of the side navigation to 250px */
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
+// /* Set the width of the side navigation */
+// function openNav() {
+//   document.getElementById("mySidenav").style.width = "340px";
+// }
 
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
+// /* Set the width of the side navigation to 0 */
+// function closeNav() {
+//   document.getElementById("mySidenav").style.width = "0";
+// }
