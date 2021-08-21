24dcce94a66d0045f30a60750082586632662319
Yui Kondo <yuikondobook@gmail.com>
2021-08-21 12:31:02 +0900

    halfway done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,21 @@
 class RestaurantsController < ApplicationController
   def index
   end
+
+  def new
+    @restaurant = Restaurant.new
+    authorize @restaurant
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    authorize @restaurant
+    @restaurant.users = current_user
+    if @restaurant.save
+      redirect_to ingredients_path
+    else
+      render :new
+    end
+  end
+
 end
